80b67acdac0ff28a2d0c881ce24ca9d1ed029ead
nagahoge <v@nanaketa.com>
2013-01-24 21:55:23 +0900

    First commit but considering going to change test framework from VisualStudio default testing tool to NUnit.

---
+++ ThumbnailBuilder/ThumbnailBuilder.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace ThumbnailBuilder {
+    /// <summary>
+    /// This class is a wrapper of creating thumbnail of image strategy.
+    /// </summary>
+    public class Thumbnail : IDisposable {
+        private Image originalImage;
+        private int thumbnailWidth;
+        private int thumbnailHeight;
+        private Color backgroundColor = Color.White;
+        private Bitmap resultImage;
+        private Graphics g;
+        private bool isBuilded = false;
+        private bool isDisposed = false;
+
+        protected Thumbnail(Image originalImage, int thumbnailWidth, int thumbnailHeight) {
+            this.originalImage = originalImage;
+            resultImage = new Bitmap(thumbnailWidth, thumbnailHeight);
+            g = Graphics.FromImage(resultImage);
+            this.thumbnailWidth = thumbnailWidth;
+            this.thumbnailHeight = thumbnailHeight;
+        }
+
+        public Graphics Graphics {
+            get { return g; }
+        }
+
+        public Color BackgroundColor {
+            get { return backgroundColor; }
+            set { backgroundColor = value; }
+        }
+
+        /// <summary>
+        /// Create a scale downed image(its x and y ratio is kept) instance.
+        /// If call this method, the caller must dispose created Image class instance after use.
+        /// </summary>
+        /// <returns></returns>
+        public Image Build() {
+            CreateThumbnail();
+            return (Image)resultImage.Clone();
+        }
+
+        /// <summary>
+        /// Create a Thumbnail class instance.
+        /// </summary>
+        /// <exception>InvalidOperationException</exception>
+        public static Thumbnail FromFile(string fileName, int thumbnailWidth, int thumbnailHeight) {
+            if (fileName == null) {
+                throw new ArgumentNullException("fileName shouldn't null.");
+            }
+            //if (fileName == null) throw new ArgumentNullException();
+            if (thumbnailWidth <= 0 || thumbnailHeight <= 0) throw new ArgumentException("size of thumbnail shouldn't less or equal to 0 value.");
+
+            try {
+                Image image = Image.FromFile(fileName);
+                return new Thumbnail(image, thumbnailWidth, thumbnailHeight);
+            } catch (OutOfMemoryException e) {
+                throw new InvalidOperationException(e.Message, e);
+            }
+        }
+
+        /// <summary>
+        /// Create a Thumbnail class instance.
+        /// Returns null value instead of thrown an exception when any error occurred.
+        /// </summary>
+        /// <returns></returns>
+        public static Thumbnail TryFromFile(string fileName, int thumbnailWidth, int thumbnailHeight) {
+            try {
+                return FromFile(fileName, thumbnailWidth, thumbnailHeight);
+            } catch {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Origin of this method was borrowed from http://www.atmarkit.co.jp/fdotnet/dotnettips/336listviewimage/listviewimage.html
+        /// </summary>
+        private void CreateThumbnail() {
+            g.FillRectangle(new SolidBrush(backgroundColor), 0, 0, thumbnailWidth, thumbnailHeight);
+
+            float fw = (float)thumbnailWidth / (float)originalImage.Width;
+            float fh = (float)thumbnailHeight / (float)originalImage.Height;
+
+            float scale = Math.Min(fw, fh);
+            fw = originalImage.Width * scale;
+            fh = originalImage.Height * scale;
+
+            g.DrawImage(originalImage, (thumbnailWidth - fw) / 2, (thumbnailHeight - fh) / 2, fw, fh);
+        }
+
+        public void Dispose() {
+            if (!isDisposed) {
+                originalImage.Dispose();
+                g.Dispose();
+                if (!isBuilded) resultImage.Dispose();
+            }
+            isDisposed = true;
+        }
+    }
+}
---
+++ ThumbnailBuilderTest/ThumbnailBuilderTest.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThumbnailBuilder;
+
+namespace ThumbnailBuilderTest
+{
+    [TestClass()]
+    public class ThumbnailBuilderTest
+    {
+        private const string red_white_1000x500 = "red_white_1000x500.png";
+        private const string yellow_black_111x333 = "yellow_black_111x333.jpg";
+        private const string gif = "sample.gif";
+        private const string tif = "sample.tif";
+        private const string bmp24bit = "sample24bit.bmp";
+        private const string bmp256colors = "sample256colors.bmp";
+        private const string bmp16colors = "sample16colors.bmp";
+        private const string bmpMonochrome = "sample_monochrome.bmp";
+        private const string notExist = "NotExist.gif";
+
+
+        private static Color red = Color.FromArgb(255, 255, 0, 0);
+        private static Color white = Color.FromArgb(255, 255, 255, 255);
+        private static Color black = Color.FromArgb(255, 0, 0, 0);
+        private static Color blue = Color.FromArgb(255, 0, 0, 255);
+        private static Color yellow = Color.FromArgb(255, 255, 255, 0);
+        
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestFromFileWithFileNameNull() {
+            
+            Thumbnail.FromFile(null, 10, 10);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestFromFileWithThumbnailWidthZero() {
+            Thumbnail.FromFile(gif, 0, 10);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestFromFileWithThumbnailWidthNegative() {
+            Thumbnail.FromFile(gif, -1, 10);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestFromFileWithThumbnailHeightZero() {
+            Thumbnail.FromFile(gif, 10, 0);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestFromFileWithThumbnailHeightNegative() {
+            Thumbnail.FromFile(gif, 10, -1);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void TestFromFileWithFileNameThatIsNotExist() {
+            Thumbnail.FromFile(notExist, 10, 10);
+        }
+
+        [TestMethod()]
+        public void TestBuild() {
+            Thumbnail builder = Thumbnail.FromFile(red_white_1000x500, 1000, 500);
+            Image image = builder.Build();
+            Assert.AreEqual(1000, image.Width);
+            Assert.AreEqual(500, image.Height);
+            Bitmap bitmap = new Bitmap(image);
+            Assert.AreEqual(Color.FromArgb(255, 255, 0, 0), bitmap.GetPixel(0, 0));
+            Assert.AreEqual(Color.FromArgb(255, 255, 0, 0), bitmap.GetPixel(0, 500 - 1));
+            Assert.AreEqual(Color.FromArgb(255, 255, 255, 255), bitmap.GetPixel(1000 - 1, 0));
+            Assert.AreEqual(Color.FromArgb(255, 255, 255, 255), bitmap.GetPixel(1000 - 1, 500 - 1));
+
+
+            // +---------------------+---------------------+
+            // |(0, 0)                             (499, 0)|
+            // |                                           |
+            // |                    blue                   |
+            // |                                           |
+            // |(0, 124)                         (499, 124)|
+            // +---------------------+---------------------+
+            // |(0, 125)   (249, 125)|(250, 125) (499, 125)|
+            // |                     |                     |
+            // |                     |                     |
+            // |                     |                     |
+            // |         red         |      white          |
+            // |                     |                     |
+            // |                     |                     |
+            // |                     |                     |
+            // |(0, 374)   (249, 374)|(250, 374) (499, 374)|
+            // +---------------------+---------------------+
+            // |(0, 375)                         (499, 375)|
+            // |                                           |
+            // |                    blue                   |
+            // |                                           |
+            // |(0, 499)                         (499, 499)|
+            // +---------------------+---------------------+
+            builder = Thumbnail.FromFile(red_white_1000x500, 500, 500);
+            builder.BackgroundColor = blue;
+            image = builder.Build();
+            Assert.AreEqual(500, image.Width);
+            Assert.AreEqual(500, image.Height);
+            bitmap = new Bitmap(image);
+
+            Assert.AreEqual(blue, bitmap.GetPixel(0, 0));
+            Assert.AreEqual(blue, bitmap.GetPixel(499, 0));
+            Assert.AreEqual(blue, bitmap.GetPixel(0, 124));
+            Assert.AreEqual(blue, bitmap.GetPixel(499, 124));
+
+            Assert.AreEqual(red, bitmap.GetPixel(0, 125));
+            Assert.AreEqual(red, bitmap.GetPixel(249, 125));
+            Assert.AreEqual(red, bitmap.GetPixel(0, 374));
+            Assert.AreEqual(red, bitmap.GetPixel(249, 374));
+
+            Assert.AreEqual(white, bitmap.GetPixel(250, 125));
+            Assert.AreEqual(white, bitmap.GetPixel(499, 125));
+            Assert.AreEqual(white, bitmap.GetPixel(250, 374));
+            Assert.AreEqual(white, bitmap.GetPixel(499, 374));
+
+            Assert.AreEqual(blue, bitmap.GetPixel(0, 375));
+            Assert.AreEqual(blue, bitmap.GetPixel(499, 375));
+            Assert.AreEqual(blue, bitmap.GetPixel(0, 499));
+            Assert.AreEqual(blue, bitmap.GetPixel(499, 499));
+
+
+            // +--------------+--------------+--------------+
+            // |(0,0)         |              |       (49, 0)|
+            // |              |              |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |    white     |      yellow  |    white     |
+            // |              |              |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |       (16,24)|(17, 24)      |              |
+            // +--------------+--------------+--------------+
+            // |      (16, 25)|(17, 25)      |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |    white     |       black  |    white     |
+            // |              |              |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |              |              |              |
+            // |(0, 49)       |              |      (49, 49)|
+            // +--------------+--------------+--------------+
+            builder = Thumbnail.FromFile(yellow_black_111x333, 50, 50);
+            image = builder.Build();
+            
+            Assert.AreEqual(50, image.Width);
+            Assert.AreEqual(50, image.Height);
+            bitmap = new Bitmap(image);
+            Assert.AreEqual(white, bitmap.GetPixel(16, 24));
+            Assert.AreEqual(yellow, bitmap.GetPixel(17, 24));
+            Assert.AreEqual(white, bitmap.GetPixel(16, 25));
+            Assert.AreEqual(black, bitmap.GetPixel(17, 25));
+        }
+
+        [TestMethod()]
+        public void TestTryFromFile() {
+            Assert.IsNotNull(Thumbnail.TryFromFile(gif, 5, 5));
+            Assert.IsNotNull(Thumbnail.TryFromFile(tif, 1, 1));
+            Assert.IsNotNull(Thumbnail.TryFromFile(bmp24bit, 10, 10));
+            Assert.IsNotNull(Thumbnail.TryFromFile(bmp256colors, 20, 20));
+            Assert.IsNotNull(Thumbnail.TryFromFile(bmp16colors, 30, 30));
+            Assert.IsNotNull(Thumbnail.TryFromFile(bmpMonochrome, 40, 40));
+            Assert.IsNotNull(Thumbnail.TryFromFile(red_white_1000x500, 10, 10));
+            Assert.IsNotNull(Thumbnail.TryFromFile(yellow_black_111x333, 10, 10));
+
+            Assert.IsNull(Thumbnail.TryFromFile("not_exist_filename.bmp", 10, 10));
+        }
+
+        // TODO test the case of specify larger size than original picture.
+        [TestMethod()]
+        public void TestExpand() {
+            Image image = Thumbnail.FromFile(gif, 10000, 10000).Build();
+        }
+    }
+}
