5ca03e2afce06d5c1b14d5438f6409729635e701
nagahoge <v@nanaketa.com>
2013-01-28 22:55:26 +0900

    Add api comments. Sandcastle can build document now.

---
+++ ThumbnailBuilder/NamespaceDoc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThumbnailBuilder {
+    /// <summary>
+    /// Just a small library only to build a thumbnail image from file or base-image.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    class NamespaceDoc {
+    }
+}
--- ThumbnailBuilder/ThumbnailBuilder.cs
+++ ThumbnailBuilder/ThumbnailBuilder.cs
@@ -21,6 +21,13 @@ namespace ThumbnailBuilder {
         private bool isBuilded = false;
         private bool isDisposed = false;
 
+        /// <summary>
+        /// Create new instance. Used from factory methods.
+        /// </summary>
+        /// <param name="baseImage">Base image of thumbnail.</param>
+        /// <param name="thumbnailWidth">Width of thumbnail.</param>
+        /// <param name="thumbnailHeight">Height of thumbnail.</param>
+        /// <exception cref="ArgumentNullException">Cause when baseImage is null.</exception>
         protected Thumbnail(Image baseImage, int thumbnailWidth, int thumbnailHeight) {
             if (thumbnailWidth <= 0 || thumbnailHeight <= 0) throw new ArgumentException("size of thumbnail shouldn't less or equal to 0 value.");
 
@@ -31,10 +38,20 @@ namespace ThumbnailBuilder {
             this.thumbnailHeight = thumbnailHeight;
         }
 
+        /// <summary>
+        /// This Graphics instance will be used on create a thumbnail when Build() method called.
+        /// So you can optimize drawing process of thumbnail by change settings of this instance.
+        /// </summary>
+        /// <return>Graphics instance.</return>
         public Graphics Graphics {
             get { return g; }
         }
 
+        /// <summary>
+        /// The color of blank area of thumbnail.
+        /// Default value is white.
+        /// Setting Color.Transparent to this parameter is also may valuable.
+        /// </summary>
         public Color BackgroundColor {
             get { return backgroundColor; }
             set { backgroundColor = value; }
@@ -44,7 +61,7 @@ namespace ThumbnailBuilder {
         /// Create a scale downed image(its x and y ratio is kept) instance.
         /// If call this method, the caller must dispose created Image class instance after use.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>An image instance of thumbnail. You should dispose this instance your own.</returns>
         public Image Build() {
             CreateThumbnail();
             return (Image)resultImage.Clone();
@@ -53,7 +70,12 @@ namespace ThumbnailBuilder {
         /// <summary>
         /// Create a Thumbnail class instance.
         /// </summary>
-        /// <exception>InvalidOperationException</exception>
+        /// <param name="fileName">Load file and used as base image.</param>
+        /// <param name="thumbnailWidth">Width of thumbnail.</param>
+        /// <param name="thumbnailHeight">Height of thumbnail.</param>
+        /// <returns>Thubmanil class instance.</returns>
+        /// <exception cref="ArgumentNullException">Cause when fileName is null.</exception>
+        /// <exception cref="ArgumentException">Cause when thubmanilWidth or thubmanilHeight is 0 or less value.</exception>
         public static Thumbnail FromFile(string fileName, int thumbnailWidth, int thumbnailHeight) {
             if (fileName == null) throw new ArgumentNullException("fileName shouldn't null.");
 
@@ -61,6 +83,15 @@ namespace ThumbnailBuilder {
             return new Thumbnail(baseImage, thumbnailWidth, thumbnailHeight);
         }
 
+        /// <summary>
+        /// Create a Thumbnail class instance from base-image.
+        /// </summary>
+        /// <param name="baseImage">Base image instance.</param>
+        /// <param name="thumbnailWidth">Width of thumbnail.</param>
+        /// <param name="thumbnailHeight">Height of thumbnail.</param>
+        /// <returns>Thubmanil class instance.</returns>
+        /// <exception cref="ArgumentNullException">Cause when baseImage is null.</exception>
+        /// <exception cref="ArgumentException">Cause when thubmanilWidth or thubmanilHeight is 0 or less value.</exception>
         public static Thumbnail FromImage(Image baseImage, int thumbnailWidth, int thumbnailHeight) {
             if (baseImage == null) throw new NullReferenceException("baseImage shouldn't null.");
             return new Thumbnail(baseImage, thumbnailWidth, thumbnailHeight);
@@ -68,9 +99,12 @@ namespace ThumbnailBuilder {
 
         /// <summary>
         /// Create a Thumbnail class instance.
-        /// Returns null value instead of thrown an exception when any error occurred.
+        /// Returns null value instead of thrown an exception when fail(=any error occurr).
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Thubmanil class instance, or null if fail to load image from fileName.</returns>
+        /// <param name="fileName">Load file and used as base image.</param>
+        /// <param name="thumbnailWidth">Width of thumbnail.</param>
+        /// <param name="thumbnailHeight">Height of thumbnail.</param>
         public static Thumbnail TryFromFile(string fileName, int thumbnailWidth, int thumbnailHeight) {
             try {
                 return FromFile(fileName, thumbnailWidth, thumbnailHeight);
@@ -95,6 +129,9 @@ namespace ThumbnailBuilder {
             g.DrawImage(baseImage, (thumbnailWidth - fw) / 2, (thumbnailHeight - fh) / 2, fw, fh);
         }
 
+        /// <summary>
+        /// Dispose all resources that Thubmanil class instance hold, but Image instance returned by Build() method.
+        /// </summary>
         public void Dispose() {
             if (!isDisposed) {
                 baseImage.Dispose();
